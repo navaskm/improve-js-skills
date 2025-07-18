2104077af64581a72ea7948df6774b8fdd391733
navaskmsanu <navasnavaskm064@gmail.com>
2025-07-17 17:47:18 -0700

    merge two number array and removes duplicates number

--- array.js
+++ array.js
@@ -1,11 +1,24 @@
 // This script finds and prints the unique (non-repeating) numbers from an array using a frequency count approach.
-const array = [1, 2, 1, 3, 1, 4, 2, 5, 4];
+const arrayOfUniq = [1, 2, 1, 3, 1, 4, 2, 5, 4];
 
-const countMap = array.reduce((acc,number)=>{
+const countMap = arrayOfUniq.reduce((acc,number)=>{
   acc[number] = (acc[number] || 0) +1;
   return acc;
 },{});
 
-const answer2 = Object.keys(countMap).filter(num => countMap[Number(num)] < 2 ).map(Number);
+const answerOfUniq = Object.keys(countMap).filter(num => countMap[Number(num)] < 2 ).map(Number);
 
-console.log(answer2);
+console.log(answerOfUniq);
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// This script merges two number arrays, removes duplicates, and sorts the result in ascending order.
+const arrayOne = [1,3,5,7,9,9,10];
+const arrayTwo = [2,4,4,6,8,10,11];
+
+const mergedArray = [...new Set([...arrayOne,...arrayTwo])];
+const sortedUniqueArray = mergedArray.sort((a, b) => a-b );
+console.log(sortedUniqueArray);
