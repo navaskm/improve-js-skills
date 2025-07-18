bb008b1705c6919c9035e9fd3675087babbed463
navaskmsanu <navasnavaskm064@gmail.com>
2025-07-17 18:48:59 -0700

    error string convert to original string

---
+++ string.js
@@ -0,0 +1,14 @@
+// This code is error string convert to original string,
+// Example: "is2 this1 book3" to "this1 is2 book3"
+const errorString = "is2 this1 book3";
+
+const wordsArray = errorString.split(" ");
+
+wordsArray.sort((word1,word2)=>{
+  const num1 = parseInt(word1.match(/\d/));
+  const num2 = parseInt(word2.match(/\d/));
+  return num1-num2;
+});
+
+const originalString = wordsArray.join(" ");
+console.log(originalString);
