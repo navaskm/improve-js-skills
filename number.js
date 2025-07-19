cac26c3baad3ecb1b13ad62117b414f6e6e4e638
navaskmsanu <navasnavaskm064@gmail.com>
2025-07-19 15:24:11 -0700

    check prime number or not

---
+++ number.js
@@ -0,0 +1,14 @@
+// check prime number or not
+const isPrime = (number) => {
+
+  if (number <= 1) return false;
+  if (number === 2) return true;
+
+  for (let i = 2; i <= Math.sqrt(number); i++) {
+    if (number % i === 0) return false;
+  }
+  return true;
+};
+
+console.log(isPrime(6));
+console.log(isPrime(7));
